7d1229a8a743a7087308316c39bdc48929292f86
bellice <bellice@tuta.io>
2024-06-18 18:29:30 +0200

    insee unzip completed

--- src/insee-00-download.py
+++ src/insee-00-download.py
@@ -46,7 +46,7 @@ def get_links_from_url(path_url, pattern):
         return links
 
     except (HTTPError, ProxyError, Timeout, RequestException) as e:
-        print(f"Erreur lors de la récupération des liens : {e}")
+        print(f"Erreur lors de la récupération des liens depuis {path_url} : {e}")
         return []
 
 def is_file_downloaded(file_name, path_output):
@@ -63,19 +63,21 @@ def is_file_downloaded(file_name, path_output):
     file_dest = path_output / file_name
     return file_dest.is_file()
 
-def download_file(link, path_output):
+def download_file(link, path_output, suffix=""):
     """
     Télécharge le fichier à partir du lien donné vers le répertoire de sortie.
 
     Parameters:
     link (str): URL du fichier à télécharger.
     path_output (Path): Chemin du répertoire de sortie.
+    suffix (str): Suffixe à ajouter au nom du fichier téléchargé.
     """
     try:
         response = requests.get(link, headers={"User-Agent": "Custom"}, proxies=proxies)
         response.raise_for_status()
 
-        file_name = Path(link).name
+        # Détermination du nom de fichier avec suffixe
+        file_name = Path(link).stem + suffix + Path(link).suffix
         file_dest = path_output / file_name
 
         if not file_dest.exists():
@@ -86,7 +88,7 @@ def download_file(link, path_output):
     except (HTTPError, ProxyError, Timeout, RequestException) as e:
         print(f"Erreur lors du téléchargement de {link} : {e}")
 
-def download_files_from_url(path_url, pattern, path_output):
+def download_files_from_url(path_url, pattern, path_output, suffix=""):
     """
     Télécharge tous les fichiers correspondant au pattern donné à partir de l'URL spécifiée.
 
@@ -94,11 +96,12 @@ def download_files_from_url(path_url, pattern, path_output):
     path_url (str): URL de la page à scrapper.
     pattern (str): Expression régulière pour matcher les liens à télécharger.
     path_output (Path): Chemin du répertoire de sortie.
+    suffix (str): Suffixe à ajouter au nom des fichiers téléchargés.
     """
     links = get_links_from_url(path_url, pattern)
 
     if not links:
-        print("Aucun lien à télécharger trouvé.")
+        print(f"Aucun lien à télécharger trouvé pour l'URL : {path_url}")
         return
 
     # Liste des fichiers déjà téléchargés
@@ -106,7 +109,7 @@ def download_files_from_url(path_url, pattern, path_output):
 
     # Filtrage des liens pour les fichiers non encore téléchargés
     links_to_download = [
-        link for link in links if Path(link).name not in downloaded_files
+        link for link in links if Path(link).stem + suffix + Path(link).suffix not in downloaded_files
     ]
 
     if not links_to_download:
@@ -116,7 +119,7 @@ def download_files_from_url(path_url, pattern, path_output):
     # Téléchargement concurrent des fichiers
     with concurrent.futures.ThreadPoolExecutor() as executor:
         futures = [
-            executor.submit(download_file, link, path_output) for link in links_to_download
+            executor.submit(download_file, link, path_output, suffix) for link in links_to_download
         ]
         for future in concurrent.futures.as_completed(futures):
             try:
@@ -128,47 +131,40 @@ def download_files_from_url(path_url, pattern, path_output):
 path_output = Path("./src/assets/insee/")
 path_output.mkdir(parents=True, exist_ok=True)  # Création du répertoire si nécessaire
 
-# Définition des URLs et patterns pour les téléchargements
-# COG 2024, 2023, 2022, 2021, 2020, 2019
-# Grille de densité 7 niveaux
-# Zonage étude Insee Aire d'attraction des villes
-# Zonage étude Insee Unité urbaine
-# Zonage étude Insee Zone d'emploi
-# Zonage étude Insee Bassin de vie
-# Population légale
-# Population Mayotte
-# Population Saint-Pierre-et-Miquelon, Saint-Barthélemy, Saint-Martin
-# Population Nouvelle-Calédonie
-# Population Polynésie française
-# Population Wallis et Futuna
-# Codification outremer (n'existe plus sur Insee, à faire soi-même)
-# Table de passage des communes
-# Table d'appartenance géographique des communes
-# Communes nouvelles
-
-urls_patterns = [
-    ("https://www.insee.fr/fr/information/7766585", "cog_ensemble_2024_csv.zip"),
-    ("https://www.insee.fr/fr/information/6800675", "cog_ensemble_2023_csv.zip"),
-    ("https://www.insee.fr/fr/information/6051727", "cog_ensemble_2022_csv.zip"),
-    ("https://www.insee.fr/fr/information/5057840", "cog_ensemble_2021_csv.zip"),
-    ("https://www.insee.fr/fr/information/4316069", "cog_ensemble_2020_csv.zip"),
-    ("https://www.insee.fr/fr/information/3720946", "cog_ensemble_2019_csv.zip"),
-    ("https://www.insee.fr/fr/information/6439600", "grille_densite_7_niveaux.*\.(xlsx|zip)$"),
-    ("https://www.insee.fr/fr/information/4803954", "AAV2020_au.*\.zip"),
-    ("https://www.insee.fr/fr/information/4802589", "UU2020_au.*\.zip"),
-    ("https://www.insee.fr/fr/information/4652957", "ZE2020_au.*\.zip"),
-    ("https://www.insee.fr/fr/information/6676988", "BV2022_au.*\.zip"),
-    ("https://www.insee.fr/fr/statistiques/6683035", "ensemble\.xlsx$"),
-    ("https://www.insee.fr/fr/statistiques/5392668", "\.xlsx$"),
-    ("https://www.insee.fr/fr/statistiques/6683025", "\.xlsx$"),
-    ("https://www.insee.fr/fr/statistiques/5392639", "\.xlsx$"),
-    ("https://www.insee.fr/fr/statistiques/6690039?sommaire=2122700", "\.xlsx$"),
-    ("https://www.insee.fr/fr/statistiques/5392492", "\.xlsx$"),
-    ("https://www.insee.fr/fr/information/7671867", "\.zip$"),
-    ("https://www.insee.fr/fr/information/7671844", "\.zip$"),
-    ("https://www.insee.fr/fr/information/2549968", "\.xls|xlsx$")
+# Définition des URLs, patterns et suffixes pour les téléchargements
+urls_patterns_suffixes = [
+    ("https://www.insee.fr/fr/information/7766585", "cog_ensemble_2024_csv.zip", ""),
+    ("https://www.insee.fr/fr/information/6800675", "cog_ensemble_2023_csv.zip", ""),
+    ("https://www.insee.fr/fr/information/6051727", "cog_ensemble_2022_csv.zip", ""),
+    ("https://www.insee.fr/fr/information/5057840", "cog_ensemble_2021_csv.zip", ""),
+    ("https://www.insee.fr/fr/information/4316069", "cog_ensemble_2020_csv.zip", ""),
+    ("https://www.insee.fr/fr/information/3720946", "cog_ensemble_2019_csv.zip", ""),
+    ("https://www.insee.fr/fr/information/6439600", "grille_densite_7_niveaux.*\.(xlsx|zip)$", ""),
+    ("https://www.insee.fr/fr/information/4803954", "AAV2020_au.*\.zip", ""),
+    ("https://www.insee.fr/fr/information/4802589", "UU2020_au.*\.zip", ""),
+    ("https://www.insee.fr/fr/information/4652957", "ZE2020_au.*\.zip", ""),
+    ("https://www.insee.fr/fr/information/6676988", "BV2022_au.*\.zip", ""),
+    ("https://www.insee.fr/fr/statistiques/7739582?sommaire=7728826", "ensemble\.xlsx$", "_pop2021"),
+    ("https://www.insee.fr/fr/statistiques/6683035", "ensemble\.xlsx$", "_pop2020"),
+    ("https://www.insee.fr/fr/statistiques/6011070?sommaire=6011075", "ensemble\.xlsx$", "_pop2019"),
+    ("https://www.insee.fr/fr/statistiques/4989724?sommaire=4989761", "ensemble\.xlsx$", "_pop2018"),
+    ("https://www.insee.fr/fr/statistiques/4265429?sommaire=4265511", "ensemble\.xls$", "_pop2017"),
+    ("https://www.insee.fr/fr/statistiques/3677785?sommaire=3677855", "ensemble\.xls$", "_pop2016"),
+    ("https://www.insee.fr/fr/statistiques/5392668?sommaire=2120838", "\.xlsx$", ""),
+    ("https://www.insee.fr/fr/statistiques/7728812?sommaire=7728826", "\.xlsx$", "_pop2021"),  
+    ("https://www.insee.fr/fr/statistiques/6683025", "\.xlsx$", "_pop2020"),
+    ("https://www.insee.fr/fr/statistiques/6011054?sommaire=6011075", "\.xlsx$", "_pop2019"),
+    ("https://www.insee.fr/fr/statistiques/4989710?sommaire=4989761", "\.xlsx$", "_pop2018"),
+    ("https://www.insee.fr/fr/statistiques/4265451?sommaire=4265511", "\.xls$", "_pop2017"),
+    ("https://www.insee.fr/fr/statistiques/3677785?sommaire=3677855", "\.xls$", "_pop2016"),
+    ("https://www.insee.fr/fr/statistiques/5392639?sommaire=2122859", "\.xlsx$", "_fr-nc"),
+    ("https://www.insee.fr/fr/statistiques/6690039?sommaire=2122700", "\.xlsx$", "_fr-pf"),
+    ("https://www.insee.fr/fr/statistiques/5392492", "\.xlsx$", "_fr-wf"),
+    ("https://www.insee.fr/fr/information/7671867", "\.zip$", ""),
+    ("https://www.insee.fr/fr/information/7671844", "\.zip$", ""),
+    ("https://www.insee.fr/fr/information/2549968", "\.xls|xlsx$", "")
 ]
 
-# Téléchargement des fichiers pour chaque URL et pattern
-for url, pattern in urls_patterns:
-    download_files_from_url(url, pattern, path_output)
+# Téléchargement des fichiers pour chaque URL, pattern et suffixe
+for url, pattern, suffix in urls_patterns_suffixes:
+    download_files_from_url(url, pattern, path_output, suffix)
--- src/laposte-00-download.py
+++ src/laposte-00-download.py
@@ -1,20 +1,47 @@
-# Importation des librairies
-from pathlib import Path
+import os
 import requests
+from requests.exceptions import ProxyError, RequestException, HTTPError, Timeout
+from dotenv import load_dotenv
 
-# Chemin
-path_output = Path("G:/cog/laposte")
+# Charger les variables d'environnement à partir du fichier .env
+load_dotenv()
 
-# Base des codes postaux - faire page séparé
-path_url = "https://koumoul.com/data-fair/api/v1/datasets/laposte-hexasmal/data-files/019HexaSmal.csv"
-r = requests.get(path_url)
+# Obtenir les informations de proxy
+proxy = os.getenv("PROXY_URL")
+proxies = {
+    "http": proxy,
+    "https": proxy
+} if proxy else None
 
-if r.status_code == 200:
-    print(f"Réponse du serveur : Succès (Code {r.status_code})")
-    with open(Path(path_output) / path_url.split("/")[-1], "wb") as f:
-        f.write(r.content)
-        print("Téléchargement réussi.")
-else:
-    print(f"Réponse du serveur : Échec (Code {r.status_code})")
+# URL du fichier CSV à télécharger
+url = "https://datanova.laposte.fr/data-fair/api/v1/datasets/laposte-hexasmal/data-files/019HexaSmal.csv"
 
+# Nom du fichier de sortie
+output_file = "./src/assets/laposte/019HexaSmal.csv"
 
+def download_csv(url, output_file, proxies=None):
+    try:
+        # Envoyer la requête GET pour télécharger le fichier
+        response = requests.get(url, proxies=proxies, timeout=10)
+        
+        # Vérifier si la requête a été réussie
+        response.raise_for_status()
+        
+        # Enregistrer le contenu dans un fichier local
+        with open(output_file, 'wb') as file:
+            file.write(response.content)
+        print(f"Fichier téléchargé avec succès et enregistré sous '{output_file}'.")
+
+    except ProxyError:
+        print("Erreur de proxy. Veuillez vérifier vos paramètres de proxy.")
+    except HTTPError as http_err:
+        print(f"Erreur HTTP: {http_err}")
+    except Timeout:
+        print("La requête a expiré. Veuillez essayer à nouveau plus tard.")
+    except RequestException as req_err:
+        print(f"Erreur lors de la requête: {req_err}")
+    except Exception as e:
+        print(f"Une erreur inattendue s'est produite: {e}")
+
+# Appeler la fonction pour télécharger le fichier
+download_csv(url, output_file, proxies=proxy)
