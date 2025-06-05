1f6daa64bff9daf79673ee67c8533ed901b33fa6
bellice <bellice@tuta.io>
2025-06-05 08:13:03 +0200

    update grpt perimetre

--- src/producers/banatic/scripts/banatic-20-table-grpt-perimetre.py
+++ src/producers/banatic/scripts/banatic-20-table-grpt-perimetre.py
@@ -18,8 +18,18 @@ file_path = os.path.join(path, file_name)
 
 # Convertir en DataFrame
 df_init = pd.read_excel(file_path)
-##################
 
+# Chargement des tables de référence INSEE
+df_com = pd.read_parquet("src/data/insee/table_com.parquet")[['com_insee', 'com_nom', 'dep_insee']]
+df_dep = pd.read_parquet("src/data/insee/table_dep.parquet")[['dep_insee', 'dep_nom']]
+
+# Jointure des tables INSEE pour avoir une table de référence complète
+df_ref = pd.merge(
+    df_com,
+    df_dep,
+    on='dep_insee',
+    how='inner'
+)
 
 # ---- 3 Transformation et nettoyage ----
 
@@ -33,17 +43,32 @@ df = (
          "Siren du membre": "grpt_membre_siren",
          "Nom du membre": "grpt_membre_nom",
          "Type du membre": "grpt_membre_type",
-         "Commune siège": "grpt_cheflieu_com"})
+         "Commune siège": "grpt_cheflieu_com",
+         "Département siège": "grpt_cheflieu_dep"})
     .pipe(lambda x: x.loc[:, x.columns.str.startswith("grpt")])
     .dropna(subset=['grpt_membre_siren'])  # Supprime les lignes où grpt_membre_siren est NULL
     .assign(
         grpt_siren=lambda x: x["grpt_siren"].astype(str),
         grpt_membre_siren=lambda x: x["grpt_membre_siren"].astype('int64').astype(str),
         grpt_interdep=lambda x: x["grpt_interdep"].map({"OUI": True, "NON": False}),
-        grpt_nom=lambda x: x["grpt_nom"].str.replace(r"\s{2,}", " ", regex=True)
+        grpt_nom=lambda x: x["grpt_nom"].str.replace(r"\s{2,}", " ", regex=True),
     )
-
 )
+
+# A METTRE EN PAUSE
+# Jointure avec la table de référence pour obtenir le code INSEE de la commune chef-lieu
+df_join = (
+    df
+    .merge(
+        df_ref,
+        left_on=['grpt_cheflieu_com', 'grpt_cheflieu_dep'],
+        right_on=['com_nom', 'dep_nom'],
+        how='left'
+    )
+    .drop(columns=['grpt_cheflieu_com', 'grpt_cheflieu_dep', 'com_nom', 'dep_nom'])
+    .rename(columns={'com_insee': 'grpt_cheflieu'})
+)
+# A FAIRE PLUS TARD, PROBLEME DE NOMBRE DE VALEURS
 
 df_epci = (
     df
@@ -51,13 +76,15 @@ df_epci = (
     .rename(columns={
             "grpt_siren": "epci_siren",
             "grpt_nom": "epci_nom",
-            "grpt_cheflieu_com": "epci_cheflieu",
+            "grpt_cheflieu_com": "epci_cheflieu_com",
+            "grpt_cheflieu_dep": "epci_cheflieu_dep",
             "grpt_naturejuridique": "epci_naturejuridique",
             "grpt_interdep": "epci_interdep",
             "grpt_membre_siren": "epci_membre_siren",
             "grpt_membre_nom": "epci_membre_nom"})
     .pipe(lambda x: x.loc[:, x.columns.str.startswith("epci")])
     )
+# 34923 df_epci
 
 df_ept = (
     df
@@ -71,7 +98,7 @@ df_ept = (
             "grpt_membre_nom": "ept_membre_nom"})
     .pipe(lambda x: x.loc[:, x.columns.str.startswith("ept")])
     )
-
+# 130 df_epci
 # ---- 4 Test d'intégrité -----
 
 try:
